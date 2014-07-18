e0ff0cd78703f6e7a8fbefeaeed61cc48138272f
Rica Zhang <ricagz@yahoo.com>
2014-07-17 22:03:46 -0700

    Chapter 11: Adding Animations to the Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -29,7 +29,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { }
+        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
         format.json { render action: 'show', status: :created, location: @cart }
       else
         format.html { render action: 'new' }
@@ -63,7 +63,7 @@ class CartsController < ApplicationController
       @cart.destroy if @cart.id == session[:cart_id]
       session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
